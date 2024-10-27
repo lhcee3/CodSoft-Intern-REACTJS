86932206be96c728d3dfa01db28189fa7fe8fdc1
Aneesh <136454406+lhcee3@users.noreply.github.com>
2024-10-27 11:04:05 +0530

    removed api credentials

--- Task 3/blogger/src/config/firebase.config.jsx
+++ Task 3/blogger/src/config/firebase.config.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDGPwgo2YlE6EJXya2sDfORW2-oqDJISc",
-  authDomain: "fir-3ca59.firebaseapp.com",
-  projectId: "fir-3ca59",
-  storageBucket: "fir-3ca59.appspot.com",
-  messagingSenderId: "664782573820",
-  appId: "1:664782573820:web:3c02658fdfeb42f5c48779",
-  measurementId: "G-MP1Z4NYWPV"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
